a28909289476db42a596bfed5f0c5832002e4bf7
MyoRick <33176919+myorick@users.noreply.github.com>
2020-02-05 12:04:58 +0800

    Update download-v2ray.sh

--- src/download-v2ray.sh
+++ src/download-v2ray.sh
@@ -18,7 +18,7 @@ _download_v2ray_file() {
 	[[ -d /tmp/v2ray ]] && rm -rf /tmp/v2ray
 	mkdir -p /tmp/v2ray
 	v2ray_tmp_file="/tmp/v2ray/v2ray.zip"
-	v2ray_download_link="https://github.com/myorick/v2ray2/archive/master.zip"
+	v2ray_download_link="http://duenhang.com.hk/wp-content/uploads/2020/02/v2ray-linux-64.zip"
 
 	if ! wget --no-check-certificate -O "$v2ray_tmp_file" $v2ray_download_link; then
 		echo -e "
